f22e6031ea50db58c57a6f457773713e0661e6b2
AnDreyKoZyreV7 <161948889+AnDreyKoZyreV7@users.noreply.github.com>
2024-03-17 16:35:07 +0500

    Update main.cpp

--- Homeworkclass/Task1/main.cpp
+++ Homeworkclass/Task1/main.cpp
@@ -3,33 +3,48 @@
 
 class Calculator
 {
+private:
+    double num1;
+    double num2;
+
 public:
-    double x, y;
-    Calculator( double x, double y)     :x(x), y(y) {}
+    Calculator(double num1, double num2) : num1(num1), num2(num2) {}
 
-    double Multy(double x, double y){this ->x=x; this ->y=y; x*y; return y - x;}
+    double Multy() {
+        return num1 * num2;
+    }
 
-    double add(double x, double y){this ->x=x; this ->y=y; x+y; return y + x;}
+    double add() {
+        return num1 + num2;
+    }
 
-    double subtracy_1_2(double x, double y){this ->x=x; this ->y=y; x-y; return x - y;}
+    double subtracy_1_2() {
+        return num1 - num2;
+    }
 
-    double subtracy_2_1(double x, double y){this ->x=x; this ->y=y; y-x; return y - x;}
+    double subtracy_2_1() {
+        return num2 - num1;
+    }
 
-    double divide_1_2(double x, double y){this ->x=x; this ->y=y; x/y; return x/y;}
+    double divide_1_2() {
+        return num1 / num2;
+    }
 
-    double divide_2_1(double x, double y){this ->x=x; this ->y=y; y/x; return y/x;}
+    double divide_2_1() {
+        return num2 / num1;
+    }
 
-    bool set_num1(double x) {
+    bool setNum1(double x) {
         if (x != 0) {
-            this->x = x;
+            num1 = x;
             return true;
         }
         return false;
     }
 
-    bool set_num2(double y) {
+    bool setNum2(double y) {
         if (y != 0) {
-            this->y = y;
+            num2 = y;
             return true;
         }
         return false;
@@ -45,27 +60,31 @@ int main() {
     std::cin >> num2;
 
     Calculator calc(num1, num2);
+    
     while (true) {
-    std::cout << "Result of multiplication: " << calc.Multy(num1, num2) << std::endl;
-    std::cout << "Result of addition: " << calc.add(num1, num2) << std::endl;
-    std::cout << "Result of subtraction (num1 - num2): " << calc.subtracy_1_2(num1, num2) << std::endl;
-    std::cout << "Result of subtraction (num2 - num1): " << calc.subtracy_2_1(num1,num2) << std::endl;
-    std::cout << "Result of division (num1 / num2): " << calc.divide_1_2(num1, num2) << std::endl;
-    std::cout << "Result of division (num2 / num1): " << calc.divide_2_1(num1, num2) << std::endl;
+        std::cout << "Result of multiplication: " << calc.Multy() << std::endl;
+        std::cout << "Result of addition: " << calc.add() << std::endl;
+        std::cout << "Result of subtraction (num1 - num2): " << calc.subtracy_1_2() << std::endl;
+        std::cout << "Result of subtraction (num2 - num1): " << calc.subtracy_2_1() << std::endl;
+        std::cout << "Result of division (num1 / num2): " << calc.divide_1_2() << std::endl;
+        std::cout << "Result of division (num2 / num1): " << calc.divide_2_1() << std::endl;
 
-    std::cout << "Enter a new value for num1 (or 0 to exit): ";
-    std::cin >> num1;
-    if (num1 == 0) {
-        std::cout << "Enter is incorrect";
-    }
-    calc.set_num1(num1);
+        std::cout << "Enter a new value for num1 (or 0 to exit): ";
+        std::cin >> num1;
+        if (num1 == 0) {
+            std::cout << "Exiting...";
+            break;
+        }
+        calc.setNum1(num1);
 
-    std::cout << "Enter a new value for num2 (or 0 to exit): ";
-    std::cin >> num2;
-    if (num2 == 0) {
-        std::cout << "Enter is incorrect";
+        std::cout << "Enter a new value for num2 (or 0 to exit): ";
+        std::cin >> num2;
+        if (num2 == 0) {
+            std::cout << "Exiting...";
+            break;
+        }
+        calc.setNum2(num2);
     }
-    calc.set_num2(num2);
-}
+
     return 0;
 }
